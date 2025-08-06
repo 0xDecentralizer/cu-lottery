3d0a901208167b049cbeefe16182d2f6c761139b
Mohammad Mahdi <MohammadMahdiKeshavarz1382@gmail.com>
2025-08-06 19:10:20 +0330

    feat: enhance Raffle contract with interval-based winner selection and state variables

--- src/Rafflel.sol
+++ src/Rafflel.sol
@@ -23,16 +23,24 @@ contract Raffle {
 	
 	/* State variables */
 	uint256 private immutable i_entranceFee;
+	/** @dev The interval is the time between two lottery. */ 
+	uint256 private i_interval;
 	address payable [] private s_players;
+	uint256 private s_lastTimestamp;
 
 	/* Events */
 	event playerEntred(address indexed player);
 
-	
 	/* Errors */
 	error Raffle__notEnoghEth();
 
 	/* Modifiers */
+
+	constructor (uint256 entranceFee, uint256 interval) {
+		i_entranceFee = entranceFee;
+		i_interval = interval;
+		s_lastTimestamp = block.timestamp;
+	}
 
 	/*  Functions */
     function enterRaffle() public payable {
@@ -43,8 +51,11 @@ contract Raffle {
 		emit playerEntred(msg.sender);
 	}
 
-    function pickWinner() public {}
-
+    function pickWinner() public {
+		if ((block.timestamp - s_lastTimestamp) < i_interval) {
+			revert();
+		}
+	}
 
 	function getEntranceFee() external view returns (uint256) {
 		return i_entranceFee;
