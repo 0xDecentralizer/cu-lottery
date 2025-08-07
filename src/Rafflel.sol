63a1912fc12ce1782cb5f7370306eb1a5f15b70c
Mohammad Mahdi <MohammadMahdiKeshavarz1382@gmail.com>
2025-08-07 23:19:58 +0330

    refactor: improve code formatting and add error handling for interval checks in Raffle contract

--- src/Rafflel.sol
+++ src/Rafflel.sol
@@ -1,16 +1,18 @@
-/** 
-	Type declarations
-	State variables
-	Events
-	Errors
-	Modifiers
-	Functions
-*/ 
+/**
+ * Type declarations
+ * 	State variables
+ * 	Events
+ * 	Errors
+ * 	Modifiers
+ * 	Functions
+ */
 
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
 /* Imports */
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.4.0/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
 /* Interfaces */
 
 /**
@@ -20,44 +22,56 @@ pragma solidity ^0.8.22;
  * @dev Implements Chainlink VRFv2.5
  */
 contract Raffle {
-	
-	/* State variables */
-	uint256 private immutable i_entranceFee;
-	/** @dev The interval is the time between two lottery. */ 
-	uint256 private i_interval;
-	address payable [] private s_players;
-	uint256 private s_lastTimestamp;
+    /* State variables */
+    uint256 private immutable i_entranceFee;
+    /**
+     * @dev The interval is the time between two lottery.
+     */
+    uint256 private i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimestamp;
 
-	/* Events */
-	event playerEntred(address indexed player);
+    /* Events */
+    event playerEntred(address indexed player);
 
-	/* Errors */
-	error Raffle__notEnoghEth();
+    /* Errors */
+    error Raffle__notEnoghEth();
+    error Raffle__intervalNotPassed();
 
-	/* Modifiers */
+    /* Modifiers */
 
-	constructor (uint256 entranceFee, uint256 interval) {
-		i_entranceFee = entranceFee;
-		i_interval = interval;
-		s_lastTimestamp = block.timestamp;
-	}
+    constructor(uint256 entranceFee, uint256 interval) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimestamp = block.timestamp;
+    }
 
-	/*  Functions */
+    /*  Functions */
     function enterRaffle() public payable {
-		if (msg.value <= i_entranceFee) {
-			revert Raffle__notEnoghEth();
-		}
-		s_players.push(payable(msg.sender));
-		emit playerEntred(msg.sender);
-	}
+        if (msg.value <= i_entranceFee) {
+            revert Raffle__notEnoghEth();
+        }
+        s_players.push(payable(msg.sender));
+        emit playerEntred(msg.sender);
+    }
 
     function pickWinner() public {
-		if ((block.timestamp - s_lastTimestamp) < i_interval) {
-			revert();
-		}
-	}
+        if ((block.timestamp - s_lastTimestamp) < i_interval) {
+            revert Raffle__intervalNotPassed();
+        }
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: enableNativePayment}))
+            })
+        );
+    }
 
-	function getEntranceFee() external view returns (uint256) {
-		return i_entranceFee;
-	}
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
