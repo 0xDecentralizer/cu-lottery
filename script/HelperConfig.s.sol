1872abd01cc7b44bbd0727e81d24be912e36bbeb
Mohammad Mahdi <MohammadMahdiKeshavarz1382@gmail.com>
2025-08-13 19:41:43 +0330

    feat: enhance HelperConfig contract with VRFCoordinator mock integration and improve local network configuration handling

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.22;
 
-contract HelperConfig {
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
     struct NetworkConfig {
         uint256 entranceFee;
         uint256 interval;
@@ -10,6 +13,10 @@ contract HelperConfig {
         uint256 subscriptionId;
         uint32 callbackGasLimit;
     }
+
+    // Mock Values
+    uint96 public constant MOCK_BASE_FEE = 0.02 ether;
+    uint96 public constant MOCK_GAS_PRICE_LINK = 1e9;
 
     uint256 public constant SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
@@ -38,15 +45,33 @@ contract HelperConfig {
 
     function getLocalConfig() public pure returns (NetworkConfig memory) {}
 
-    function getConfigByChainId(uint256 chainId) public view returns (NetworkConfig memory) {
+    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
         if (networkConfigs[chainId].vrfCoordinator != address(0)) {
             return networkConfigs[chainId];
         } else if (chainId == LOCAL_CHAIN_ID) {
-            return localNetworkConfig;
+            return getOrCreateAnvilEthConfig();
         } else {
             revert HelperConfig__InvalidChainId();
         }
     }
 
-    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {}
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (localNetworkConfig.vrfCoordinator != address(0)) {
+            return localNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vrfCoordinatorV2Mock = new VRFCoordinatorV2Mock(MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK);
+        vm.stopBroadcast();
+
+        return NetworkConfig
+        ({
+            entranceFee: 0.02 ether,
+            interval: 30,
+            vrfCoordinator: address(vrfCoordinatorV2Mock),
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            subscriptionId: 0,
+            callbackGasLimit: 500000
+        });
+    }
 }
