93571abd4d8e5d7a4551950ace789350113b2a61
Mohammad Mahdi <MohammadMahdiKeshavarz1382@gmail.com>
2025-08-13 19:01:13 +0330

    refactor: improve code formatting and simplify function definitions in HelperConfig contract

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -13,30 +13,30 @@ contract HelperConfig {
 
     uint256 public constant SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
-    
+
     error HelperConfig__InvalidChainId();
-    
+
     NetworkConfig private localNetworkConfig;
-    mapping (uint256 chainId => NetworkConfig) public networkConfigs;
+    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
 
     constructor() {
         networkConfigs[SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
     }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
-        return(NetworkConfig({
-            entranceFee: 0.02 ether,
-            interval: 30,
-            vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
-            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
-            subscriptionId: 0,
-            callbackGasLimit: 500000
-        }));
+        return (
+            NetworkConfig({
+                entranceFee: 0.02 ether,
+                interval: 30,
+                vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+                gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+                subscriptionId: 0,
+                callbackGasLimit: 500000
+            })
+        );
     }
 
-    function getLocalConfig() public pure returns (NetworkConfig memory) {
-
-    }
+    function getLocalConfig() public pure returns (NetworkConfig memory) {}
 
     function getConfigByChainId(uint256 chainId) public view returns (NetworkConfig memory) {
         if (networkConfigs[chainId].vrfCoordinator != address(0)) {
@@ -48,9 +48,5 @@ contract HelperConfig {
         }
     }
 
-    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
-
-    }
- 
-
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {}
 }
