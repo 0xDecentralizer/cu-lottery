5337215ea3edea7fc30750d9f8089786a7c27b7a
Mohammad Mahdi <MohammadMahdiKeshavarz1382@gmail.com>
2025-08-13 18:48:31 +0330

    feat: add Sepolia network configuration to HelperConfig contract

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.22;
 
 contract HelperConfig {
+    uint256 constant SEPOLIA_CHAIN_ID = 11155111;
+
     struct NetworkConfig {
         uint256 entranceFee;
         uint256 interval;
@@ -14,10 +16,19 @@ contract HelperConfig {
     NetworkConfig private localNetworkConfig;
     mapping (uint256 chainId => NetworkConfig) public networkConfigs;
 
-    constructor() {}
+    constructor() {
+        networkConfigs[SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
+    }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
-
+        return(NetworkConfig({
+            entranceFee: 0.02 ether,
+            interval: 30,
+            vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            subscriptionId: 0,
+            callbackGasLimit: 500000
+        }));
     }
 
     function getLocalConfig() public pure returns (NetworkConfig memory) {
